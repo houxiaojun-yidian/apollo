12eeec42b2c3417832c20ecb8ae2cf4ff32cd17d
侯晓君 <houxiaojun@yidian-inc.com>
2019-03-01 14:53:59 +0800

    merge完成后消掉了application.yml中的ldap相关属性值以及build.sh中数据库Ip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://10.103.16.194:3306/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_url=jdbc:mysql://127.0.0.1:3306/ApolloConfigDB?characterEncoding=utf8
 apollo_config_db_username=root
 apollo_config_db_password=123456
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://10.103.16.194:3306/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_url=jdbc:mysql://127.0.0.1:3306/ApolloPortalDB?characterEncoding=utf8
 apollo_portal_db_username=root
 apollo_portal_db_password=123456
 
 # meta server url, different environments should have different meta server addresses
-dev_meta=http://10.103.16.194:8080
-fat_meta=http://10.103.16.194:8080
-uat_meta=http://10.103.16.194:8080
-pro_meta=http://10.103.16.194:8080
+dev_meta=http://127.0.0.1:8080
+fat_meta=http://127.0.0.1:8080
+uat_meta=http://127.0.0.1:8080
+pro_meta=http://127.0.0.1:8080
 
 META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
 
